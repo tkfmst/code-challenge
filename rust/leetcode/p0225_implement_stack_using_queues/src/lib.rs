849f6aba09065ddf2122a52b2c45df5286fe06da
takafumi-s <takafumi.s.dev@gmail.com>
2022-02-18 20:11:29 +0900

    Solve LeetCode problem225 `Implement Stack using Queues`

---
+++ rust/leetcode/p0225_implement_stack_using_queues/src/lib.rs
@@ -0,0 +1,80 @@
+#![allow(dead_code)]
+/// 225. Implement Stack using Queues
+/// Easy
+///
+/// Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+///
+/// Implement the MyStack class:
+/// * void push(int x) Pushes element x to the top of the stack.
+/// * int pop() Removes the element on the top of the stack and returns it.
+/// * int top() Returns the element on the top of the stack.
+/// * boolean empty() Returns true if the stack is empty, false otherwise.
+///
+/// Notes:
+/// * You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+/// * Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+///
+/// Example 1:
+///
+/// Input
+/// ["MyStack", "push", "push", "top", "pop", "empty"]
+/// [[], [1], [2], [], [], []]
+/// Output
+/// [null, null, null, 2, 2, false]
+///
+/// Explanation
+/// MyStack myStack = new MyStack();
+/// myStack.push(1);
+/// myStack.push(2);
+/// myStack.top(); // return 2
+/// myStack.pop(); // return 2
+/// myStack.empty(); // return False
+///
+///
+///
+/// Constraints:
+///
+/// * 1 <= x <= 9
+/// * At most 100 calls will be made to push, pop, top, and empty.
+/// * All the calls to pop and top are valid.
+
+struct MyStack {
+    stack: Vec<i32>,
+}
+
+impl MyStack {
+    fn new() -> Self {
+        Self { stack: vec![] }
+    }
+
+    fn push(&mut self, x: i32) {
+        self.stack.push(x)
+    }
+
+    fn pop(&mut self) -> i32 {
+        self.stack.pop().unwrap()
+    }
+
+    fn top(&self) -> i32 {
+        *self.stack.last().unwrap()
+    }
+
+    fn empty(&self) -> bool {
+        self.stack.is_empty()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::MyStack;
+    #[test]
+    fn test_stack() {
+        let mut obj = MyStack::new();
+        obj.push(1);
+        obj.push(2);
+
+        assert_eq!(obj.top(), 2);
+        assert_eq!(obj.pop(), 2);
+        assert_eq!(obj.empty(), false);
+    }
+}
