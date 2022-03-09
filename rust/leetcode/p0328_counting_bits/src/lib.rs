36ed0e44a3fa0badf7ebd442bc269724a4711038
takafumi-s <takafumi.s.dev@gmail.com>
2022-03-09 09:21:22 +0900

    Solve LeetCode problem338

---
+++ rust/leetcode/p0328_counting_bits/src/lib.rs
@@ -0,0 +1,64 @@
+/// 338. Counting Bits
+/// Easy
+///
+/// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+///
+///
+/// Example 1:
+///
+/// Input: n = 2
+/// Output: [0,1,1]
+/// Explanation:
+/// 0 --> 0
+/// 1 --> 1
+/// 2 --> 10
+///
+/// Example 2:
+///
+/// Input: n = 5
+/// Output: [0,1,1,2,1,2]
+/// Explanation:
+/// 0 --> 0
+/// 1 --> 1
+/// 2 --> 10
+/// 3 --> 11
+/// 4 --> 100
+/// 5 --> 101
+///
+///
+/// Constraints:
+/// * 0 <= n <= 10^5
+///
+/// Follow up:
+/// * It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
+/// * Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
+pub struct Solution {}
+
+impl Solution {
+    pub fn count_bits(n: i32) -> Vec<i32> {
+        let mut acc = vec![];
+        for i in 0..(n + 1) {
+            acc.push(i.count_ones() as i32);
+        }
+        acc
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn test_count_bits_1() {
+        let n = 2;
+        let output = vec![0, 1, 1];
+        assert_eq!(Solution::count_bits(n), output);
+    }
+
+    #[test]
+    fn test_count_bits_2() {
+        let n = 5;
+        let output = vec![0, 1, 1, 2, 1, 2];
+        assert_eq!(Solution::count_bits(n), output);
+    }
+}
